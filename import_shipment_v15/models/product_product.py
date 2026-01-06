72b7ff4f109fc44c082b09372bdd8026749302fa
TugayHatil <tugay.hatill@gmail.com>
2026-01-06 22:59:19 +0300

    Add x_manufacturer_code to fix validation error

--- import_shipment_v15/models/product_product.py
+++ import_shipment_v15/models/product_product.py
@@ -4,3 +4,4 @@ class ProductProduct(models.Model):
     _inherit = 'product.product'
 
     manufacturer_pref = fields.Char(string='Manufacturer Pref', help="Code used for Import Shipment matchmaking (FIFO).")
+    x_manufacturer_code = fields.Char(related='manufacturer_pref', string='X Manufacturer Code', readonly=False, store=True)
